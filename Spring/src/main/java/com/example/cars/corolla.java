7ef656b44abef68bc1c4b15a6c49a6ca8a9f743d
Taimoor-8 <106242227+Taimoor-8@users.noreply.github.com>
2023-06-20 12:36:25 +0500

    Add files via upload

---
+++ Spring/src/main/java/com/example/cars/corolla.java
@@ -0,0 +1,11 @@
+package com.example.cars;
+
+import com.example.interfaces.car;
+
+public class corolla implements car {
+
+    @Override
+    public String specs() {
+        return "corolla specs";
+    }
+}
